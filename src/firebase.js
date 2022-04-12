bb08d580dc789903fe527d648d21ff8fd1f4e437
stoneboat <tood2111@gmail.com>
2022-04-12 23:45:48 +0900

    firebase 초기화 파일 작성

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0gKKTkNd-ddrUWjT3W8xxZJlEiUVvYjs",
+  authDomain: "quest-75b46.firebaseapp.com",
+  projectId: "quest-75b46",
+  storageBucket: "quest-75b46.appspot.com",
+  messagingSenderId: "502350063576",
+  appId: "1:502350063576:web:6eadd46062f3af8879f4ad",
+  measurementId: "G-G9J9N82JKD",
+};
+
+export default initializeApp(firebaseConfig);
