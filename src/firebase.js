28617db2320dc1ed4a87787f3feaef5223afd002
stoneboat <tood2111@gmail.com>
2022-04-13 00:02:57 +0900

    firebase 환경변수 작성

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0gKKTkNd-ddrUWjT3W8xxZJlEiUVvYjs",
-  authDomain: "quest-75b46.firebaseapp.com",
-  projectId: "quest-75b46",
-  storageBucket: "quest-75b46.appspot.com",
-  messagingSenderId: "502350063576",
-  appId: "1:502350063576:web:6eadd46062f3af8879f4ad",
-  measurementId: "G-G9J9N82JKD",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default initializeApp(firebaseConfig);
