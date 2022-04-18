acbc9070d3f342f2f0f1ff420bf2ffe6ed73094d
stoneboat <tood2111@gmail.com>
2022-04-18 14:15:34 +0900

    구글 로그인 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import AppRouter from "./components/AppRouter";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { authService } from "./fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : ""}</>;
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
+import { authService } from "../fbase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = (event) => {
     console.log(event.target.name);
@@ -15,8 +22,30 @@ function Auth() {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      // setError(error.message);
+    }
+  };
+
+  const onClickSocialLogin = async () => {
+    let provider = new GoogleAuthProvider();
+    const data = await signInWithPopup(authService, provider);
+    console.log(data);
   };
 
   return (
@@ -38,10 +67,13 @@ function Auth() {
           value={password}
           onChange={onChange}
         />
-        <input type='submit' value='Log In' />
+        <input
+          type='submit'
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
       </form>
       <div>
-        <button>Sign in with Google</button>
+        <button onClick={onClickSocialLogin}>Sign in with Google</button>
       </div>
     </div>
   );
