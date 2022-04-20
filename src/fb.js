d397d5580f89422bf2550a935b4d038c1d6be60f
nurimeansworld <nurimeansworld@gmail.com>
2022-04-20 18:14:03 +0900

    :wrench: .env 파일로 firebase config 수정

--- src/fb.js
+++ src/fb.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCk78A9oKNmOvXFKNtaWWruvIuobMw9bTw',
-  authDomain: 'quest-8e2cd.firebaseapp.com',
-  projectId: 'quest-8e2cd',
-  storageBucket: 'quest-8e2cd.appspot.com',
-  messagingSenderId: '344007212165',
-  appId: '1:344007212165:web:b0cfbcb782a1bf98363dec',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
