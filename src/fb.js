43a697813fac1b1c03acb72ad00a879bd8c68251
nurimeansworld <nurimeansworld@gmail.com>
2022-04-20 18:10:30 +0900

    :seedling: Set: firebase 초기 세팅(#1)

---
+++ src/fb.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCk78A9oKNmOvXFKNtaWWruvIuobMw9bTw',
+  authDomain: 'quest-8e2cd.firebaseapp.com',
+  projectId: 'quest-8e2cd',
+  storageBucket: 'quest-8e2cd.appspot.com',
+  messagingSenderId: '344007212165',
+  appId: '1:344007212165:web:b0cfbcb782a1bf98363dec',
+};
+
+export default firebase.initializeApp(firebaseConfig);
